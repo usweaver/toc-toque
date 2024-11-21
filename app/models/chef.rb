42a33df9ec420de71dd369e42e8388cf1724afe6
Florian CHAPUIS <fchapuis20@gmail.com>
2024-11-21 14:13:50 +0100

    Searchbar configured

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,12 @@ class Chef < ApplicationRecord
   has_one_attached :chef_picture
   belongs_to :user
   has_many :bookings
+
+  include PgSearch::Model
+  pg_search_scope :search_chef,
+  against: [ :first_name, :last_name, :city, :category ],
+  using: {
+    tsearch: { prefix: true }
+  }
+
 end
